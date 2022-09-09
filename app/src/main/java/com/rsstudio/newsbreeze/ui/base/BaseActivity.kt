e08dded702290a9e35fbb82422c8a41cdc8653f7
Rishi Srivastava <official.rishisrivastava@example.com>
2022-09-10 04:35:26 +0530

    ui made

--- app/src/main/java/com/rsstudio/newsbreeze/ui/base/BaseActivity.kt
+++ app/src/main/java/com/rsstudio/newsbreeze/ui/base/BaseActivity.kt
@@ -1,10 +1,12 @@
 package com.rsstudio.newsbreeze.ui.base
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.rsstudio.newsbreeze.R
 import com.rsstudio.newsbreeze.databinding.ActivityBaseBinding
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -15,5 +17,9 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_base)
+    }
+
+    override fun attachBaseContext(context: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(context))
     }
 }
